552c44fec59d5a5515a941b46300c02c14cdee3b
enea-scaccabarozzi <enea.scaccabarozzi@apuliasoft.com>
2024-10-25 09:01:11 +0200

    chore: initial setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,70 @@
-import { Slot } from "expo-router";
+import { Slot, SplashScreen } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Theme, ThemeProvider } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { Platform } from "react-native";
+
+import { NAV_THEME } from "@/shared/constants";
+import { useColorScheme } from "@/shared";
+
+import "../assets/global.css";
+
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+// Prevent the splash screen from auto-hiding before getting the color scheme.
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem("theme");
+      if (Platform.OS === "web") {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add("bg-background");
+      }
+      if (!theme) {
+        AsyncStorage.setItem("theme", colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === "dark" ? "dark" : "light";
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
+      SplashScreen.hideAsync();
+    });
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
-      <Slot />
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+        <Slot />
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
---
+++ libs/shared/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_THEME = {
+  light: {
+    background: "hsl(0 0% 100%)", // background
+    border: "hsl(20 5.9% 90%)", // border
+    card: "hsl(0 0% 100%)", // card
+    notification: "hsl(0 84.2% 60.2%)", // destructive
+    primary: "hsl(24.6 95% 53.1%)", // primary
+    text: "hsl(240 10% 3.9%)", // foreground
+  },
+  dark: {
+    background: "hsl(20 14.3% 4.1%)", // background
+    border: "hsl(12 6.5% 15.1%)", // border
+    card: "hsl(20 14.3% 4.1%)", // card
+    notification: "hsl(0 72% 51%)", // destructive
+    primary: "hsl(20.5 90.2% 48.2%)", // primary
+    text: "hsl(0 0% 98%)", // foreground
+  },
+};
